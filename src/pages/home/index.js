acd216ffb7704efb557ffb4f42196aa92402271d
AnbolKing <272344341@qq.com>
2021-04-24 14:27:18 +0800

    test: 完善home页，package.json修改，准备build

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import {
+  Link,
+} from 'react-router-dom';
+import './style.less';
 
 const Home = () => {
   return (
-    <div>HOME</div>
+    <div className="home-container">
+      <Link to='/hookDrag'>
+        <div className="home-button">
+          hookDrag
+        </div>
+      </Link>
+      <Link to='/lightInput'>
+        <div className="home-button">
+          lightInput
+        </div>
+      </Link>
+      <Link to='/infiniteScroll'>
+        <div className="home-button">
+          infiniteScroll
+        </div>
+      </Link>
+      <Link to='/masonryLayout'>
+        <div className="home-button">
+          masonryLayout
+        </div>
+      </Link>
+    </div>
   )
 }
 
